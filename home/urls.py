97ec0f99746db72f5f618b70c14efe2dd5152ac6
hectorjesusra <63563445+Hectorjesusra@users.noreply.github.com>
2020-07-12 22:42:43 -0500

    Ready to use- first release

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('lab', views.lab, name='lab'),
+    path('lab/about', views.lababout, name='lababout'),
+    path('lab/apps', views.labapps, name='labapps'),
+    path('lab/apps/molemule', views.molemule, name='molemule'),
+    path('lab/apps/nucleo', views.nucleo, name='nucleo'),
+    path('lab/apps/pyteins', views.pyteins, name='pyteins'),
+    path('lab/tutorial', views.labtutorial, name='labtutorial'),
     path('contact', views.contact, name='contact'),
     path('forum', views.forum, name='forum'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,22 +2,34 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-
-    content={'name':'Hector','age':18}
-    return render(request, 'index.html',content)
+    return render(request, 'index.html')
 
 def about(request):
-
     return render(request, 'about.html')
 
 def lab(request):
-
     return render(request, 'lab.html')
 
-def contact(request):
+def lababout(request):
+    return render(request, 'lab/about.html')
 
+def labapps(request):
+    return render(request, 'lab/apps.html')
+
+def molemule(request):
+    return render(request, 'lab/apps/molemule.html')
+
+def nucleo(request):
+    return render(request, 'lab/apps/nucleo.html')
+
+def pyteins(request):
+    return render(request, 'lab/apps/pyteins.html')
+
+def labtutorial(request):
+    return render(request, 'lab/tutorial.html')
+
+def contact(request):
     return render(request, 'contact.html')
 
 def forum(request):
-
     return render(request, 'forum.html')
