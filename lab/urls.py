9f2b0a65b44f6814098adfe22e79c5d0bdd8addc
hectorjesusra <63563445+Hectorjesusra@users.noreply.github.com>
2020-07-17 15:48:15 -0500

    s

--- lab/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from lab import views
-
-urlpatterns = [
-    path('about', views.about, name='about'),
-    path('apps', views.apps, name='apps'),
-    path('apps/', include('labapps.urls'), name='apps'),
-    path('tutorial', views.tutorial, name='tutorial'),
-]
--- labapps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from labapps import views
-
-urlpatterns = [
-    path('molemule', views.molemule, name='molemule'),
-    path('nucleo', views.nucleo, name='nucleo'),
-    path('pyteins', views.pyteins, name='pyteins'),
-]
